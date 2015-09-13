55eb5d7cd875acc40fff60fe4655fdd806b41f66
Alex Vaske <avaske25@gmail.com>
2015-09-13 16:19:30 -0500

    Modified temp_conv.sh to include Kelvin

--- temp_conv.sh
+++ temp_conv.sh
@@ -4,9 +4,12 @@ printf "Please enter a Fahrenheit Temperature. I will convert it to degrees Celc
 read F
 
 C=`echo "($F-32)*5/9"|bc -l` 
+K=`echo "$C+273.15"|bc -l`
 echo "$F degrees Fahrenheit is"
-printf %.1f $C
+printf %.2f $C
 echo " degrees Celcius"
+printf %.2f $K
+echo " Kelvin"
 
 
 
